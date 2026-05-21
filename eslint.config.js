f4ca7a4b45c509c6730e131e6412b27d2c4c2f4e
Muhammad Ammar Asad <ammarasad321993@gmail.com>
2026-05-21 19:19:20 +0500

    chore: ignore maps-scrape directory in eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'android', 'scratch', 'public']),
+  globalIgnores(['dist', 'android', 'scratch', 'public', 'maps-scrape']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
